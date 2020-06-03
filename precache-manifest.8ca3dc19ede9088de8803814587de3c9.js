c84fb65a4d581cee23488139ba5244fe03f2ef51
LLIaJIyHuLLIKa <vlad.tokan96@mail.ru>
2020-06-03 10:33:12 +0300

    Updates

---
+++ precache-manifest.8ca3dc19ede9088de8803814587de3c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cde24a6f5307fdb902dbfa00e482569",
+    "url": "/reactBlog/index.html"
+  },
+  {
+    "revision": "2b67aba84a20f8bb29fd",
+    "url": "/reactBlog/static/css/main.66310705.chunk.css"
+  },
+  {
+    "revision": "8a5e55ec2dab33bc870a",
+    "url": "/reactBlog/static/js/2.08bb29df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactBlog/static/js/2.08bb29df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b67aba84a20f8bb29fd",
+    "url": "/reactBlog/static/js/main.58b786ec.chunk.js"
+  },
+  {
+    "revision": "9431533c09eb753b190c",
+    "url": "/reactBlog/static/js/runtime-main.d96334e4.js"
+  },
+  {
+    "revision": "36a237d62b62356782bbbd87901020f7",
+    "url": "/reactBlog/static/media/background.36a237d6.jpg"
+  }
+]);
